de90f39698416abe72738f26e0001a59c74dc723
shimobayashi <akimasashimobayashi@gmail.com>
2018-01-05 14:17:04 +0900

    フィードを吐くエンドポイントを分離した。

--- app.rb
+++ app.rb
@@ -8,52 +8,52 @@ class AddGuid < Sinatra::Base
   enable :show_exceptions
 
   get '/' do
-    unless params[:url]
-      haml :index
-    else
-      feed = FeedNormalizer::FeedNormalizer.parse open(params[:url])
-      feed.entries.each do |entry|
-        # guidに相当する文字列を渡されたオプションからよしなに生成する。
-        seed = ''
-        if params[:link]
-          seed += entry.url
-        end
-        if params[:title]
-          seed += entry.title
-        end
-        if params[:description]
-          seed += entry.content
-        end
-        if params[:date]
-          seed += entry.date_published.to_s
-        end
-        if params[:guid]
-          seed += entry.id ? entry.id : ''
-        end
+    haml :index
+  end
 
-        # entry.idを存否の確認をせずに上書きする。
-        entry.id = Digest::MD5.hexdigest(seed)
+  get '/feed' do
+    feed = FeedNormalizer::FeedNormalizer.parse open(params[:url])
+    feed.entries.each do |entry|
+      # guidに相当する文字列を渡されたオプションからよしなに生成する。
+      seed = ''
+      if params[:link]
+        seed += entry.url
+      end
+      if params[:title]
+        seed += entry.title
+      end
+      if params[:description]
+        seed += entry.content
+      end
+      if params[:date]
+        seed += entry.date_published.to_s
+      end
+      if params[:guid]
+        seed += entry.id ? entry.id : ''
       end
 
-      rss = RSS::Maker.make('2.0') do |rss|
-        rss.channel.title = feed.title
-        rss.channel.description = feed.description
-        rss.channel.link = feed.url
-
-        feed.entries.each do |entry|
-          item = rss.items.new_item
-          item.title = entry.title
-          item.link = entry.url
-          item.guid.content = entry.id
-          # 元のフィードでisPermaLinkがどうだったかは保存されていない気がする(要出典)ので、一律でfalseにしておく
-          item.guid.isPermaLink = false
-          item.description = entry.content
-          item.date = entry.date_published
-        end
-      end
-
-      content_type 'application/rss+xml', :charset => 'utf-8'
-      rss.to_s
+      # entry.idを存否の確認をせずに上書きする。
+      entry.id = Digest::MD5.hexdigest(seed)
     end
+
+    rss = RSS::Maker.make('2.0') do |rss|
+      rss.channel.title = feed.title
+      rss.channel.description = feed.description
+      rss.channel.link = feed.url
+
+      feed.entries.each do |entry|
+        item = rss.items.new_item
+        item.title = entry.title
+        item.link = entry.url
+        item.guid.content = entry.id
+        # 元のフィードでisPermaLinkがどうだったかは保存されていない気がする(要出典)ので、一律でfalseにしておく
+        item.guid.isPermaLink = false
+        item.description = entry.content
+        item.date = entry.date_published
+      end
+    end
+
+    content_type 'application/rss+xml', :charset => 'utf-8'
+    rss.to_s
   end
 end
